f5452fcd4e2ff426f6033e0c7072f4a4e7359fa0
Ratnesh-Dhan <ratneshdhan@gmail.com>
2024-10-27 02:15:19 +0530

    added purge directories route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 from .indexFiles import indexFiles_bp
 from .query import query_bp
 from .upload import upload_bp
+from .purge import  purge_bp
 
 def init_routes(app):
     app.register_blueprint(indexFiles_bp)
     app.register_blueprint(query_bp)
     app.register_blueprint(upload_bp)
+    app.register_blueprint(purge_bp)
